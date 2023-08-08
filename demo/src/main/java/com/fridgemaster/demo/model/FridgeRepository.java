0da2876b013c0a50cc5c4271d02aef26ddcf01c3
Tomi1325 <113603371+Tomi1325@users.noreply.github.com>
2023-08-08 13:57:30 +0200

    make method names consistent

--- demo/src/main/java/com/fridgemaster/demo/model/FridgeRepository.java
+++ demo/src/main/java/com/fridgemaster/demo/model/FridgeRepository.java
@@ -4,6 +4,7 @@ package com.fridgemaster.demo.model;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,7 +12,30 @@ import java.util.UUID;
 public class FridgeRepository {
     List<Fridge> fridges;
 
-    public Optional<Fridge> getFridgeContent(UUID id){
-        return fridges.stream().filter(c -> c.getId().equals(id)).findFirst();
+    public List<Item> getFridgeItem(UUID id){
+      Optional< Fridge> fridge = fridges.stream().filter(c -> c.getId().equals(id)).findFirst();
+
+      if(fridge.isPresent()){
+
+      return fridge.get().getFridgeItems();
+      }
+      throw new NoSuchElementException("No fridge with this ID");
+    }
+
+    public void addItemToFridge(UUID fridgeID,Item item){
+        Optional< Fridge> fridge = fridges.stream().filter(c -> c.getId().equals(fridgeID)).findFirst();
+
+        if(fridge.isPresent()){
+            fridge.get().addItemToFridge(item);
+        }
+    }
+
+    public void deleteItemFromFridge(UUID fridgeID, Item item){
+        Optional< Fridge> fridge = fridges.stream().filter(c -> c.getId().equals(fridgeID)).findFirst();
+
+        if(fridge.isPresent()){
+            fridge.get().deleteItemFromFridge(item);
+        }
+
     }
 }
