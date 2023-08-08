03499714801112be7003c1f1181c4da044bf193b
Tomi1325 <113603371+Tomi1325@users.noreply.github.com>
2023-08-08 10:35:27 +0200

    create springBootApp

---
+++ demo/src/test/java/com/fridgemaster/demo/FridgemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fridgemaster.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgemasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
