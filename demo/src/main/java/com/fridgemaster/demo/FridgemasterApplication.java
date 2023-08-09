80ab29e0c2aa73dc5ea864c5abbf4bda9511b744
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-08-09 11:51:05 +0200

    delete unused imports

--- demo/src/main/java/com/fridgemaster/demo/FridgemasterApplication.java
+++ demo/src/main/java/com/fridgemaster/demo/FridgemasterApplication.java
@@ -1,15 +1,12 @@
 package com.fridgemaster.demo;
 
-import com.fridgemaster.demo.model.FridgeRepository;
-import com.fridgemaster.demo.service.FridgeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FridgemasterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FridgemasterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FridgemasterApplication.class, args);
+    }
 }
